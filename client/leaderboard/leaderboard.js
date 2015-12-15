5732c238ed12ad827caf745c857187097938ec44
Alexis Kovalenko <alexis.kovalenko@gmail.com>
2015-12-15 11:32:00 +0100

    Mise à jour du leaderboard (style + filtrage)

--- client/leaderboard/leaderboard.js
+++ client/leaderboard/leaderboard.js
@@ -1,8 +1,9 @@
 Template.leaderboard.helpers({
   users: function(){
-    return Meteor.users.find({}, {sort: {'profile.score': -1}}).fetch();
+    //return only valid users (i.e. that have a username)
+    return Meteor.users.find({'profile.name' : {$exists: 1}}, {sort: {'profile.score': -1}}).fetch();
   },
   lastGoodAnswer: function(){
-    return Meteor.users.findOne({}, {sort: {'profile.lastgoodanswer': -1}}).profile.name;
+    return '<span class="animated linear bounceInRight">' + Meteor.users.findOne({}, {sort: {'profile.lastgoodanswer': -1}}).profile.name + '</span>';
   }
 });
